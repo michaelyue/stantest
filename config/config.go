2839379d3f34707b25e27effe4f67cdfe1854263
Michael <463329348@qq.com>
2025-04-19 12:09:35 +1000

    default port changed to 80

--- config/config.go
+++ config/config.go
@@ -4,6 +4,6 @@ import "github.com/labstack/gommon/log"
 
 const (
 	// DEFAULT_PORT is the default port for the API server
-	DEFAULT_PORT    = "8089"
+	DEFAULT_PORT    = "80"
 	LOG_LEVEL_DEBUG = log.DEBUG
 )
